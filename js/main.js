c49a7d196cd7eefc6c48420317f13d2af6df4cc8
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2019-01-08 18:47:08 -0500

    Add shooting, auto starting, bugfix.

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ window.onkeyup = function(event) {
 }
 
 var enemyBullets = [];
+var playerBullets = [];
+var playerBulletCooldown = 0;
 
 var player = {
 	x: 0,
@@ -30,8 +32,8 @@ var enemies = [
 		size: .75, 
 		health: 20,
 		isBoss: true,
-		moves: [0, 1],
-		doingMove: false
+		moves: [0, 1, 2],
+		doingMove: true
 	}
 ]
 
@@ -42,6 +44,20 @@ var boss_moves = [
 	function(boss, frame) {
 		if (frame % 3 == 0) {
 			var theta = 2 * Math.PI * frame / 100
+			var x = Math.cos(theta);
+			var y = Math.sin(theta);
+			enemyBullets.push({
+				x: boss.x + 0.25 * x,
+				y: boss.y + 0.25 * y,
+				deltaX: x / 2,
+				deltaY: y / 2,
+				damage: 10
+			});
+		}
+	},
+	function(boss, frame) {
+		if (frame % 3 == 0) {
+			var theta = -2 * Math.PI * frame / 100
 			var x = Math.cos(theta);
 			var y = Math.sin(theta);
 			enemyBullets.push({
@@ -92,6 +108,17 @@ function tick() {
 	if (keys["ArrowRight"] && player.x < 11) {
 		player.x += 0.2
 	}
+	playerBulletCooldown--;
+	if (keys["Space"] && playerBulletCooldown < 0) {
+		playerBullets.push({
+			x: player.x,
+			y: player.y,
+			deltaX: 0,
+			deltaY: -0.5,
+			damage: 5
+		})
+		playerBulletCooldown = 200;
+	}
 	if (activeMoves.length) {
 		activeMoves = activeMoves.map(move => {
 			move.f(move.b, move.frames);
@@ -102,6 +129,7 @@ function tick() {
 			}
 		}).filter(x=>x);
 	}
+
 	var size = canvas.getBoundingClientRect();
 	canvas.width = size.width;
 	canvas.height = size.height;
@@ -110,26 +138,29 @@ function tick() {
 	var unit = Math.min(canvas.width, canvas.height)/32;
 	// used for scaling so that every screen size gets the same game.
 
+	// end pre-calculations, start drawing
 	context.fillStyle = "black";
 	context.beginPath();
 	context.arc(player.x * unit, player.y * unit, unit/2, 0, 2 * Math.PI);
 	context.fill();
 
 	enemies = enemies.map(enemy => {
-		context.fillStyle = "green";
-		context.beginPath();
-		context.arc(enemy.x * unit, enemy.y * unit, enemy.size * unit, 0, 2 * Math.PI);
-		context.fill();
-		if (enemy.isBoss && !enemy.doingMove && 0.01 > Math.random()) {
-			enemy.doingMove = true;
-			activeMoves.push({
-				b: enemy,
-				f: boss_moves[enemy.moves[Math.floor(Math.random() * enemy.moves.length)]],
-				frames:  100
-			});
+		if (enemy.health > 0) {
+			context.fillStyle = "green";
+			context.beginPath();
+			context.arc(enemy.x * unit, enemy.y * unit, enemy.size * unit, 0, 2 * Math.PI);
+			context.fill();
+			if (enemy.isBoss && !enemy.doingMove && 0.01 > Math.random()) {
+				enemy.doingMove = true;
+				activeMoves.push({
+					b: enemy,
+					f: boss_moves[enemy.moves[Math.floor(Math.random() * enemy.moves.length)]],
+					frames:  100
+				});
+			}
+			return enemy
 		}
-		return enemy
-	})
+	}).filter(x=>x);
 
 	enemyBullets = enemyBullets.map(bullet=>{
 		bullet.x += bullet.deltaX
@@ -151,7 +182,31 @@ function tick() {
 		return bullet
 	}).filter(x=>x);
 
+	playerBullets = playerBullets.map(bullet=>{
+		bullet.x += bullet.deltaX
+		bullet.y += bullet.deltaY
+
+		context.fillStyle = "crimson";
+		context.beginPath();
+		context.arc(bullet.x * unit, bullet.y * unit, unit/8, 0, 2 * Math.PI);
+		context.fill();
+		if (Math.abs(bullet.x) > 11 || Math.abs(bullet.y) > 16) {
+			return null
+		}
+		var bulletUsed = false;
+		enemies = enemies.map(enemy => {
+			if (!bulletUsed && sq(bullet.x - enemy.x) + sq(bullet.y - enemy.y) < sq(enemy.size + .125)) { // 25/64 = (.5 + .125) ** 2
+				console.log("enemy collision!");
+				enemy.health -= bullet.damage;
+				bulletUsed = true;
+			}
+			return enemy
+		})
+		if (!bulletUsed)
+			return bullet
+	}).filter(x=>x);
+
 	window.requestAnimationFrame(tick)
 }
 
-//tick();
+tick();
